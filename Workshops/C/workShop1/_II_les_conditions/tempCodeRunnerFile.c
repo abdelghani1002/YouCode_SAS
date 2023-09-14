38f07955c158448f9fe3b7d1a0ed180c6595f657
abdelghaniAit <aaittamghart8@gmail.com>
2023-09-14 16:35:04 +0100

    II_challenge_7

---
+++ Workshops/C/workShop1/_II_les_conditions/tempCodeRunnerFile.c
@@ -0,0 +1,9 @@
+// stocke le code ASCII de caractere 
+    myCharASCII = myChar;
+
+    // Vérifie le caractere s'il est un alphabet majuscule
+    if(myCharASCII >= 65 && myCharASCII <= 90){
+        printf("\nLa caractere %c est un alphabet majuscule.", myChar);
+    }else{
+        printf("\nLa caractere %c n'est pas un alphabet majuscule.", myChar);
+    }
