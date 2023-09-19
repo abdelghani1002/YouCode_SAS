ef97e702c0c58696d65e3fb4c2037d247167eae1
abdelghaniAit <aaittamghart8@gmail.com>
2023-09-19 18:26:19 +0100

    IIII_challenge_2

--- C/workShop1/_IIII_Tableaux/challenge_2.c
+++ C/workShop1/_IIII_Tableaux/challenge_2.c
@@ -11,14 +11,14 @@ int main(){
     int myArray[10], min, max, i = 0;
 
     // Initializing min and max by the first element 
-    printf("\n\tEnter element myArray[0] : ");
+    printf("\n\t\tEnter element myArray[0] : ");
     scanf("%d", &myArray[0]);
     max = myArray[0];
     min = myArray[0];
 
     // Get the rest of the array
     for(i = 1; i < 10; i++){
-        printf("\tEnter element myArray[%d] : ", i);
+        printf("\t\tEnter element myArray[%d] : ", i);
         scanf("%d", &myArray[i]);
         if(min > myArray[i]) min = myArray[i];
         if(max < myArray[i]) max = myArray[i];
@@ -26,10 +26,10 @@ int main(){
 
     // Output the array elements
     for(int i = 0; i < 10; i++){
-        printf("\tmyArray[%d] = %d\n",i, myArray[i]);
+        printf("\t\tmyArray[%d] = %d\n",i, myArray[i]);
     }
 
     // Output the max and min
-    printf("\t\tLe plus petit est : %d\n", min);
-    printf("\t\tLe plus grand est : %d\n", max);
+    printf("\t\t\tLe plus petit est : %d\n", min);
+    printf("\t\t\tLe plus grand est : %d\n", max);
 }
